9e90ea67dfe651ff9479b002d926acde8d1a301f
Andrespeerez <andreslavila93@gmail.com>
2026-02-17 09:26:29 +0100

    doc: add documentation to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,7 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Class that represents the Category of a Product
+ * 
+ * e.g. Chair, Table
+ */
 class Category extends Model
 {
     protected $fillable = ['name', 'active'];
@@ -12,7 +18,11 @@ class Category extends Model
         'active' => 'boolean',
     ];
 
-    public function products() {
+    /**
+     * Return the collection of products that has this category assigned
+     * @return HasMany<Product, Category>
+     */
+    public function products() : HasMany {
         return $this->hasMany(Product::class);
     }
 }
