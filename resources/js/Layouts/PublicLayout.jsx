c8e0b20aa10a8d45f916246950a3ca0eb2bcf548
Andrespeerez <andreslavila93@gmail.com>
2026-02-19 15:04:28 +0100

    feat: create PublicLayout for all pages (Header, Footer and menus still needs to be implemented)

---
+++ resources/js/Layouts/PublicLayout.jsx
@@ -0,0 +1,17 @@
+import { Link } from "@inertiajs/react";
+import { useState } from "react";
+
+export default function PublicLayout ({ children, auth }) {
+    const [ menuOpen, setMenuOpen ] = useState(false);
+    const [ cartOpen, setCartOpen ] = useState(false);
+
+    return (
+        <div className="min-h-screen flex-col bg-neutral-50">
+            <Header />
+            <main className="flex-1">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
+}
