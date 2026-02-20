3f4b5224508a25d212ab4ea48e4d706a3f086836
Andrespeerez <andreslavila93@gmail.com>
2026-02-20 11:15:30 +0100

    feat: implement navbar on footer (links don't point anywhere rightnow)

--- resources/js/Components/Public/Footer.jsx
+++ resources/js/Components/Public/Footer.jsx
@@ -1,7 +1,22 @@
+import { Link } from "@inertiajs/react";
+
 export default function Footer() {
     return (
-        <footer>
-            Mi footer
+        <footer
+        className="flex justify-center items-center py-[60px] bg-neutral-700 text-neutral-50"
+        >
+            <section>
+                <h2 className="heading-6 text-center">Navegación:</h2>
+                <nav
+                className="flex flex-col gap-4 items-center"
+                >
+                    <Link href="/sobre-nosotros">Sobre nosotros</Link>
+                    <Link href="/contacto">Contacto</Link>
+                    <Link href="/aviso-legal">Aviso Legal</Link>
+                    <Link href="/cookies">Cookies</Link>
+                    <Link href="/proteccion-datos">Protección de datos</Link>
+                </nav>
+            </section>
         </footer>
     );
 }
