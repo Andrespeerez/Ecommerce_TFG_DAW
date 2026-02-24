a5901ab3d21931329c09cab6bb298dfce0ee2610
Andrespeerez <andreslavila93@gmail.com>
2026-02-24 15:30:07 +0100

    feat: profile page and form that allows user to change account details

---
+++ resources/js/Pages/Public/UserProfile.jsx
@@ -0,0 +1,25 @@
+import UserProfileEditInfo from "@/Components/Public/UserProfileEditInfo";
+import PublicLayout from "@/Layouts/PublicLayout";
+import { Head } from "@inertiajs/react";
+
+// TODO: This is a fake cart. Need a real one from server
+const cartDefault = {
+    total_items: 0
+}
+
+export default function UserProfile({ cart = cartDefault, auth, canResetPassword, categories, mustVerifyEmail, status }) {
+    return (
+        <>
+            <Head title="Área Cliente " />
+            
+            <PublicLayout cart={cart} auth={auth} canResetPassword={canResetPassword} categories={categories}>
+                <aside>
+
+                </aside>
+                <div>
+                    <UserProfileEditInfo mustVerifyEmail={mustVerifyEmail} status={status} /> 
+                </div>
+            </PublicLayout>
+        </>
+    );
+}
