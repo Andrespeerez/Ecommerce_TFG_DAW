46728689299acaaf59fbcf07026ea844f5aa57d1
Andrespeerez <andreslavila93@gmail.com>
2026-02-24 20:11:20 +0100

    style: add styles to aside bar

--- resources/js/Pages/Public/UserProfile.jsx
+++ resources/js/Pages/Public/UserProfile.jsx
@@ -1,6 +1,7 @@
 import UserProfileAccount from "@/Components/Public/UserProfileAccount";
+import UserProfileInfo from "@/Components/Public/UserProfileInfo";
 import PublicLayout from "@/Layouts/PublicLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 
 // TODO: This is a fake cart. Need a real one from server
 const cartDefault = {
@@ -13,12 +14,23 @@ export default function UserProfile({ cart = cartDefault, auth, canResetPassword
             <Head title="Área Cliente " />
             
             <PublicLayout cart={cart} auth={auth} canResetPassword={canResetPassword} categories={categories}>
-                <aside>
-
-                </aside>
-                <div>
-                    <UserProfileAccount mustVerifyEmail={mustVerifyEmail} status={status} /> 
+                <div
+                className="flex gap-5"
+                >
+                    <aside
+                    className="md:w-1/4 lg:w-1/5 hidden md:block"
+                    >
+                        <ul className="flex flex-col gap-2 text-left pl-4 pt-4 heading-6">
+                            <li><Link href="/area-cliente" className="text-neutral-600">Datos de Cliente</Link></li>
+                            <li><Link href="/area-cliente/pedidos">Pedidos</Link></li>
+                        </ul>
+                    </aside>
+                    <div className="grow flex flex-col gap-3 mx-4">
+                        <UserProfileAccount mustVerifyEmail={mustVerifyEmail} status={status} /> 
+                        <UserProfileInfo mustVerifyEmail={mustVerifyEmail} status={status} /> 
+                    </div>
                 </div>
+                
             </PublicLayout>
         </>
     );
