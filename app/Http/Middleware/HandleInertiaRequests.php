398f3bf3e4cf5082b5dedddc9beba95e86c35ffe
Andrespeerez <andreslavila93@gmail.com>
2026-02-25 13:31:27 +0100

    feat: Create cart as a session variable and returns an object as a prop with all cart items.

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Inertia\Middleware;
@@ -30,7 +31,71 @@ class HandleInertiaRequests extends Middleware
      * @return array<string, mixed>
      */
     public function share(Request $request): array
-    {
+    {   
+        // 1 Initialize Cart if doesn't exists
+        if (!session()->has('cart')) {
+            session(['cart' => []]);
+        }
+
+        /*
+        cart session store something like this:
+        cart = [
+            1 => ['product_id' => 1, 'quantity' => 2],
+            ...
+        ]
+
+        cart prop should have a structure like this:
+        cart = {
+            items : [
+                { 
+                    data: { id: 2, name: "...", price: ... },
+                    quantity: ..., 
+                    errors: [...] 
+                },
+                { 
+                    data: {id: 5, name: "...", price: ..., } 
+                    quantity: ...,
+                    errors: [...] 
+                }, ...
+            ],
+            total_items : ...,
+            errors: [...],
+        }
+        */
+
+        // 2 Object to return as a prop
+        $cart = session('cart', []);
+
+        $cartItems = empty($cart) ? collect() : 
+            Product::whereIn('id', array_keys($cart))->get()
+                ->map(function($product) use ($cart) {
+                    $quantity = $cart[$product->id]['quantity'];
+                    $itemErrors = [];
+
+                    if (!$product->active) {
+                        $itemError[] = 'El producto ya no está disponible';
+                    }
+
+                    if (!$quantity > $product->stock) {
+                        $itemError[] = 'No hay stock suficiente';
+                    }
+
+                    return [
+                        'data' => $product,
+                        'quantity' => $quantity,
+                        'errors' => $itemError,
+                    ];
+                });
+
+        
+        $totalItems = $cartItems->sum('quantity');
+        $totalPrice = $cartItems->sum(fn($item) => $item['data']->price_with_iva * $item['quantity']);
+
+        $cartErrors = [];
+        if ($totalItems > 20) {
+            $cartErrors[] = 'Tienes demasiados productos en el carrito';
+        }
+
         return [
             ...parent::share($request),
             'auth' => [
@@ -41,6 +106,12 @@ class HandleInertiaRequests extends Middleware
                           ->where('active', true)
                           ->get();
             }),
+            'cart' => [
+                'items' => $cartItems,
+                'total_items' => $totalItems,
+                'total_price' => $totalPrice,
+                'errors' => $cartErrors,
+            ],
         ];
     }
 }
