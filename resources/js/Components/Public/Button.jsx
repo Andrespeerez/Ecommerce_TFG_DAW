c106b7ab818808b4f36a8d7297554ad88c35fe64
Andrespeerez <andreslavila93@gmail.com>
2026-02-26 11:48:32 +0100

    style: change secondary button color to bg-secondary-700, because it needs more contrast ratio

--- resources/js/Components/Public/Button.jsx
+++ resources/js/Components/Public/Button.jsx
@@ -2,8 +2,8 @@ import { Link } from '@inertiajs/react';
 
 const variants = {
     'primary': "bg-primary-700 hover:bg-primary-500 active:bg-primary-900 text-primary-50 transition duration-150 md:heading-5 text-base rounded-lg px-5 py-3 flex justify-center items-center",
-    'secondary': "bg-secondary-500 hover:bg-secondary-700 active:bg-secondary-900 text-primary-50 transition duration-150 md:heading-6 text-base rounded-lg px-5 py-3 flex justify-center items-center",
-    'accent': "bg-accent-500 hover:bg-accent-700 active:bg-accent-900 text-primary-50 transition duration-150 md:heading-6 text-base rounded-lg px-5 py-3 flex justify-center items-center",
+    'secondary': "bg-secondary-700 hover:bg-secondary-500 active:bg-secondary-900 text-primary-50 transition duration-150 md:heading-6 text-base rounded-lg px-5 py-3 flex justify-center items-center",
+    'accent': "bg-accent-700 <hover:bg-accent-5></hover:bg-accent-5>00 active:bg-accent-900 text-primary-50 transition duration-150 md:heading-6 text-base rounded-lg px-5 py-3 flex justify-center items-center",
 };
 
 export default function Button ({ type = 'button', children, onClick, disabled = false, variant = 'primary', className = '' }) {
