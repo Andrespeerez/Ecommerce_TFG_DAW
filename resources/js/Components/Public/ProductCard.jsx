114e815893bf08b20ae660854a37f587effa3aef
Andrespeerez <andreslavila93@gmail.com>
2026-02-26 12:54:33 +0100

    chore: add schema.org itemScope to ProductCard

--- resources/js/Components/Public/ProductCard.jsx
+++ resources/js/Components/Public/ProductCard.jsx
@@ -12,23 +12,32 @@ export default function ProductCard({ product = productFake }) {
 
     return (
         <article
+        itemScope 
+        itemType="https://schema.org/Product"
         aria-label={`Tarjeta de producto: "${product.name}"`}
         className="rounded-[20px] flex flex-col justify-between gap-3 pb-[10px] bg-neutral-300 max-w-[450px] w-full overflow-hidden border-t-1 lg:h-[600px] md:h-[500px]"
         >
             <img src={`/storage/${product.image_url}`} alt={`Foto de ${product.name}`} 
             className="object-cover w-full h-[70%]"
+            itemProp="image"
             />
 
             <h3
             className="heading-6 lg:heading-5 text-primary-900 text-center"
+            itemProp="name"
             >
                 {product.name}
             </h3>
 
             <div
             className="flex md:flex-row flex-col gap-4 justify-evenly items-center px-2"
+            itemProp="offers" 
+            itemScope 
+            itemType="https://schema.org/Offer"
             >
+                <meta itemprop="priceCurrency" content="EUR" />
                 <div
+                itemProp="price"
                 className="text-primary-900 heading-6 lg:heading-5"
                 >
                     {product.price_with_iva}€
@@ -39,7 +48,12 @@ export default function ProductCard({ product = productFake }) {
                 </ButtonLink>
             </div>
 
+            {product.description && (
+                <meta itemProp="description" content={product.description} />
+            )}
+
 
         </article>
     );
 }
+
