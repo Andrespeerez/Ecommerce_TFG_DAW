5113a0069145f50e48721a8f321c7836dcef3741
Andrespeerez <andreslavila93@gmail.com>
2026-02-26 12:08:12 +0100

    fix: add arial-label to loginbutton

--- resources/js/Components/Public/LoginButton.jsx
+++ resources/js/Components/Public/LoginButton.jsx
@@ -3,8 +3,9 @@ export default function LoginButton({ onClick }) {
         <button
         onClick={onClick}
         className="bg-neutral-50 hover:bg-neutral-300 active:bg-neutral-500 flex gap-[10px] items-center px-[10px] py-[5px] rounded-[10px] transition duration-150"
+        aria-label="Menu de Login"
         >       
-            <img src="/assets/images/login.svg" alt="Menu Login" />
+            <img src="/assets/images/login.svg" alt="Menu Login" aria-label="Menu de Login"/>
 
             <span className="hidden md:inline text-primary-900 heading-6">Login</span>
         </button>
