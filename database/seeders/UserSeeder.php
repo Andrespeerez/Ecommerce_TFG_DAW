03ac8564d071155c1fe033f19bc992dbcc213c2d
Andrespeerez <andreslavila93@gmail.com>
2026-02-28 16:03:39 +0100

    chore: change seeder for users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'email' => 'admin@barberes.com',
+            'email' => 'admin@ecommerce.com',
             'password' => 'admin',
             'full_name' => 'Administrador del Sitio',
             'phone' => '',
