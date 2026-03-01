6a58736120df59fd94c038bbb95460128c7381aa
Andrespeerez <andreslavila93@gmail.com>
2026-03-01 11:27:25 +0100

    doc: add documentation to the SecurityHeaders

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -18,23 +18,59 @@ class SecurityHeaders
     {
         $response = $next($request);
 
+        /**
+         * X-Frame-Options: DENY
+         * 
+         * Impide que mi web sea cargada dentro de un <iframe> en otra página
+         * Un atacante puede meter mi web dentro de un iframe y correr un script que 
+         * capture las teclas de teclado o clicks del ratón para robarme datos
+         */
         $response->headers->set('X-Frame-Options', 'DENY');
 
+        /**
+         * X-Content-Type-Options: nosniff
+         * 
+         * Obliga al navegador a respetar el MIME del archivo.
+         * Si un archivo contiene un script oculto en el código, el navegador lo
+         * tratará como lo que indique el MIME y no ejecutará el script malicioso
+         */
         $response->headers->set('X-Content-Type-Options', 'nosniff');
 
+        /**
+         * Referrer-Policy: strict-origin-when-cross-origin
+         * 
+         * Evita enviar la ruta completa desde la que vienes (rutas internas) al
+         * viajar a una ruta externa
+         */
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
 
+        /**
+         * Cross-Origin-Opener-Policy: same-origin
+         * 
+         * Aisla el contenido de mi web par evitar fgas de información
+         */
         $response->headers->set('Cross-Origin-Opener-Policy', 'same-origin');
 
+        /**
+         * Permissions-Policy: camera=(), microphone=(), geolication()
+         * 
+         * Restrige el acceso a los dispositivos del usuario
+         */
         $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
 
+        /**
+         * Content-Security-Policy (CSP)
+         * 
+         * Define que recursos puede cargar el navegador
+         * (solo aplicado a producción, en localhost causan problemas) 
+         */
         if (App::environment('production')) {
-            $csp = "default-src 'self'; " .
-                   "script-src 'self' 'unsafe-inline'; " . 
-                   "style-src 'self' 'unsafe-inline'; " .
-                   "img-src 'self' data: https://ecommerce-tfgdaw-production.up.railway.app; " .
-                   "connect-src 'self'; " .
-                   "upgrade-insecure-requests;";
+            $csp = "default-src 'self'; " .  // solo recursos de mi dominio
+                   "script-src 'self' 'unsafe-inline'; " .  // Scripts propios y cosas de inertia/ziggy
+                   "style-src 'self' 'unsafe-inline'; " .  // css mio y estilos inyectados por tailwind
+                   "img-src 'self' data: https://ecommerce-tfgdaw-production.up.railway.app; " . // permite fotos que vengan de mi dominio
+                   "connect-src 'self'; " . // peticiones fetch solo a mi servidor
+                   "upgrade-insecure-requests;"; // todas las peticiones son convertidas a HTTPS
 
             $response->headers->set('Content-Security-Policy', $csp);
         }
