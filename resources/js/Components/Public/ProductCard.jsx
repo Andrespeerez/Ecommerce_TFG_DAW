6081f4f6b399f0ef828517167e2ee630e2b61cc4
Andrespeerez <andreslavila93@gmail.com>
2026-03-02 19:15:51 +0100

    feat: add Skeletons to product cards

--- resources/js/Components/Public/ProductCard.jsx
+++ resources/js/Components/Public/ProductCard.jsx
@@ -1,6 +1,7 @@
 import { router, usePage } from "@inertiajs/react";
 import Button, {  } from "./Button";
 import { useState } from "react";
+import Skeleton from "react-loading-skeleton";
 
 // FAKE PRODUCT
 const productFake = {
@@ -12,8 +13,9 @@ const productFake = {
 
 export default function ProductCard({ product = productFake }) {
     const [ isAdding, setIsAdding ] = useState(false);
+    const [ isLoading, setIsLoading ] = useState(true);
     const { cart } = usePage().props;
-
+    
     const handleClickCard = (e) => {
         router.visit(route('products.show', product.id));
     }
@@ -45,11 +47,19 @@ export default function ProductCard({ product = productFake }) {
         onClick={handleClickCard}
         >
             <figure className="group relative w-full h-[70%] overflow-hidden rounded-lg">
+                {isLoading && 
+                <Skeleton 
+                
+                className="w-full h-full"
+                />
+                }
+
                 <img 
                     src={`/storage/${product.image_small_url}`} alt={`Foto de ${product.name}`} 
-                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                    className={`w-full h-full object-cover transition-transform duration-500 group-hover:scale-110 ${isLoading ? "opacity-0" : "opacity-100"}`}
                     itemProp="image"
                     loading="lazy"
+                    onLoad={() => setIsLoading(false)}
                 />
             </figure>            
 
