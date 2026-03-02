d3491d7323ed130c7df5dd6d07050cc7249df8ac
Andrespeerez <andreslavila93@gmail.com>
2026-03-02 18:58:07 +0100

    fix: remove routes that I don't use

--- routes/auth.php
+++ routes/auth.php
@@ -12,14 +12,14 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+    //Route::get('register', [RegisteredUserController::class, 'create'])
+    //    ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store'])
         ->middleware('throttle:3,1');
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    //Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    //    ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store'])
         ->middleware('throttle:3,1');
@@ -49,8 +49,8 @@ Route::middleware('auth')->group(function () {
         ->middleware('throttle:6,1')
         ->name('verification.send');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
+    // Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+    //     ->name('password.confirm');
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
