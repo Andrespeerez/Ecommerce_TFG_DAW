70ba4935bdd6878026c90a71c339c3aaca7a01f2
Andrespeerez <andreslavila93@gmail.com>
2026-03-03 14:18:20 +0100

    feat: add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @viteReactRefresh
+        <link rel="icon" type="image/svg+xml" href="/assets/images/logo2.svg">
 
         <link rel="preload" as="image" href="/assets/images/logo2.svg" fetchpriority="high" type="image/svg+xml">
         <link rel="preload" as="image" href="/assets/images/categories.svg" fetchpriority="high" type="image/svg+xml">
@@ -16,13 +14,17 @@
         <link rel="preload" as="image" href="/assets/images/login.svg" fetchpriority="high" type="image/svg+xml">
         <link rel="preload" as="image" href="/assets/images/cart.svg" fetchpriority="high" type="image/svg+xml">
 
+        <!-- Scripts -->
+        @routes
+        @viteReactRefresh
+
         @if(Route::currentRouteName() === 'home.index')
             <link rel="preload" as="image" href="/assets/images/hero.webp" fetchpriority="high" type="image/webp">
         @endif
 
         @if($page['component'] === 'Errors/Construction')
             <link rel="preload" as="image" href="/assets/images/under-construction.webp" fetchpriority="high" type="image/webp">
-        @endif
+        @endif        
         
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
